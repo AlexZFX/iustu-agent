534f5979c588895edcbb72ad42c8547b19e68d8d
AlexZFX <z1079911968@163.com>
2018-05-28 13:12:55 +0800

    initial commit
    (cherry picked from commit 85e240a)

---
+++ src/main/java/com/iustu/agentdemo/AgentDemoApplication.java
@@ -0,0 +1,12 @@
+package com.iustu.agentdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgentDemoApplication.class, args);
+    }
+}
