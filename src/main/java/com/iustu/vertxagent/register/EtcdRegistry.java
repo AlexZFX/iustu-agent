9b97eac4ad154cddc766b47a5066f29e6fc8ed21
AlexZFX <z1079911968@163.com>
2018-06-10 21:27:08 +0800

    [modify] 添加根据size 注册，随机负载均衡

--- src/main/java/com/iustu/vertxagent/register/EtcdRegistry.java
+++ src/main/java/com/iustu/vertxagent/register/EtcdRegistry.java
@@ -12,6 +12,7 @@ import org.slf4j.LoggerFactory;
 
 import java.text.MessageFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.Executors;
 
@@ -55,10 +56,11 @@ public class EtcdRegistry implements IRegistry {
 
     // 向ETCD中注册服务
     public void register(String serviceName, int port) throws Exception {
+        String size = System.getProperty("size");
         // 服务注册的key为:    /dubbomesh/com.some.package.IHelloService/192.168.100.100:2000
         String strKey = MessageFormat.format("/{0}/{1}/{2}:{3}", rootPath, serviceName, IpHelper.getHostIp(), String.valueOf(port));
         ByteSequence key = ByteSequence.fromString(strKey);
-        ByteSequence val = ByteSequence.fromString("");     // 目前只需要创建这个key,对应的value暂不使用,先留空
+        ByteSequence val = ByteSequence.fromString(size);     // 目前只需要创建这个key,对应的value暂不使用,先留空
         kv.put(key, val, PutOption.newBuilder().withLeaseId(leaseId).build()).get();
         logger.info("Register a new service at:" + strKey);
     }
@@ -88,14 +90,27 @@ public class EtcdRegistry implements IRegistry {
 
         for (com.coreos.jetcd.data.KeyValue kv : response.getKvs()) {
             String s = kv.getKey().toStringUtf8();
+            String size = kv.getValue().toStringUtf8();
             int index = s.lastIndexOf("/");
             String endpointStr = s.substring(index + 1, s.length());
 
             String host = endpointStr.split(":")[0];
             int port = Integer.valueOf(endpointStr.split(":")[1]);
-
-            endpoints.add(new Endpoint(host, port));
+            if ("medium".equals(size)) {
+                endpoints.add(new Endpoint(host, port));
+                endpoints.add(new Endpoint(host, port));
+                endpoints.add(new Endpoint(host, port));
+            } else if ("large".equals(size)) {
+                endpoints.add(new Endpoint(host, port));
+                endpoints.add(new Endpoint(host, port));
+                endpoints.add(new Endpoint(host, port));
+                endpoints.add(new Endpoint(host, port));
+                endpoints.add(new Endpoint(host, port));
+            } else {
+                endpoints.add(new Endpoint(host, port));
+            }
         }
+        Collections.shuffle(endpoints);
         return endpoints;
     }
 }
