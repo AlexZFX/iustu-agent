7d53c1dd4e69e9201a37b8ed4ec4cd5be8d2ad37
AlexZFX <z1079911968@163.com>
2018-06-12 14:14:43 +0800

    [modify] 修改CommonHolder

--- src/main/java/com/iustu/vertxagent/dubbo/model/CommonHolder.java
+++ src/main/java/com/iustu/vertxagent/dubbo/model/CommonHolder.java
@@ -1,29 +1,32 @@
 package com.iustu.vertxagent.dubbo.model;
 
 import io.netty.channel.Channel;
-import io.netty.util.Attribute;
+import io.netty.channel.ChannelId;
 import io.netty.util.AttributeKey;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class CommonHolder {
 
     // key: requestId     value: CommonFuture
     public static final AttributeKey<Map<Long, CommonFuture>> rpcFutureMapKey = AttributeKey.valueOf("CommonFutureMap");
 
-    public static final Object lock = new Object();
+    private static final ConcurrentHashMap<ChannelId, HashMap<Long, CommonFuture>> channelMap = new ConcurrentHashMap<>();
+
+//    public static final Object lock = new Object();
 
     public static void registerFuture(Channel channel, long requestId, CommonFuture commonFuture) {
-        Attribute<Map<Long, CommonFuture>> attr = channel.attr(rpcFutureMapKey);
-        Map<Long, CommonFuture> commonFutureMap = attr.get();
-        if (commonFutureMap == null) {
-            synchronized (lock) {
-                commonFutureMap = new HashMap<>();
-                attr.setIfAbsent(commonFutureMap);
-            }
-        }
-        commonFutureMap.put(requestId, commonFuture);
+        HashMap<Long, CommonFuture> futureMap = channelMap.computeIfAbsent(channel.id(), k -> new HashMap<>());
+        futureMap.put(requestId, commonFuture);
+//        Attribute<Map<Long, CommonFuture>> attr = channel.attr(rpcFutureMapKey);
+//        Map<Long, CommonFuture> commonFutureMap = attr.get();
+//        if (commonFutureMap == null) {
+//            commonFutureMap = new HashMap<>();
+//            attr.setIfAbsent(commonFutureMap);
+//        }
+//        commonFutureMap.put(requestId, commonFuture);
 //        CommonFuture oldFuture = commonFutureMap.put(requestId, commonFuture);
 //        if (oldFuture != null) {
 //            throw new IllegalStateException("requestId " + requestId + " exists");
@@ -31,14 +34,14 @@ public class CommonHolder {
     }
 
     public static CommonFuture getAndRemoveFuture(Channel channel, long requestId) {
-        Attribute<Map<Long, CommonFuture>> attr = channel.attr(rpcFutureMapKey);
-        Map<Long, CommonFuture> futureMap = attr.get();
-        if (futureMap == null) {
-            synchronized (lock) {
-                futureMap = new HashMap<>();
-                attr.setIfAbsent(futureMap);
-            }
-        }
+        HashMap<Long, CommonFuture> futureMap = channelMap.computeIfAbsent(channel.id(), k -> new HashMap<>());
         return futureMap.remove(requestId);
+//        Attribute<Map<Long, CommonFuture>> attr = channel.attr(rpcFutureMapKey);
+//        Map<Long, CommonFuture> futureMap = attr.get();
+//        if (futureMap == null) {
+//            futureMap = new HashMap<>();
+//            attr.setIfAbsent(futureMap);
+//        }
+//        return futureMap.remove(requestId);
     }
 }
